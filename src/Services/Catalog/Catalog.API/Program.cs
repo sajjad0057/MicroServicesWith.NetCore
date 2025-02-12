db6ecc364e5c62f0c7410f1539f88ae8fb23e127
sajjad0057 <sajjadhossain0057@gmail.com>
2025-02-12 19:23:03 +0600

    update launchsettings.json file

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+#region Configuring DI
+
+#endregion
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+#region Configuring Request pipe-line
+
+#endregion
 
 app.Run();
