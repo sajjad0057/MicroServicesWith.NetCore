7600b28e7cf8c3e7931bc65bbadb6a0635fd2fd7
sajjad0057 <sajjadhossain0057@gmail.com>
2025-02-12 21:39:00 +0600

    Created Cammand and Handler for create product intially

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public sealed record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : IRequest<CreateProductResult>;
+
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        //// Business Logic to create a product
+        throw new NotImplementedException();
+    }
+}
