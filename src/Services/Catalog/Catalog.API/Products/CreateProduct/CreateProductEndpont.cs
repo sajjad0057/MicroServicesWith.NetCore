b43d509cb0868dec97e5816c4bb4707f5d722fd1
sajjad0057 <sajjadhossain0057@gmail.com>
2025-02-13 00:55:51 +0600

    Uisng Carter Library create a endpoint for making post request with product

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpont.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpont.cs
@@ -1,5 +1,36 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using Carter;
+using Mapster;
+using MediatR;
 
-public class CreateProductEndpont
+namespace Catalog.API.Products.CreateProduct;
+
+public sealed record CreateProductRequest(string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
+
+public sealed record CreateProductResponse(Guid Id);
+
+
+public class CreateProductEndpont : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<CreateProductResponse>();
+
+            return Results.Created($"/products/{response.Id}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
+    }
 }
