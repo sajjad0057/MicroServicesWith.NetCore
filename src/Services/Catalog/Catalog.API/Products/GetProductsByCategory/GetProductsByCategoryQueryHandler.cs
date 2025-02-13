810331893991eb65afe0a02c4c8b02b7b15d046d
sajjad0057 <sajjadhossain0057@gmail.com>
2025-02-13 23:35:01 +0600

    updated misspelling

--- src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryQueryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryQueryHandler.cs
@@ -3,14 +3,15 @@
 public class GetProductsByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductsByCategoryQueryHandler> logger)
     : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryQueryResult>
 {
-    public async Task<GetProductsByCategoryQueryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+    public async Task<GetProductsByCategoryQueryResult> Handle(GetProductsByCategoryQuery query, 
+        CancellationToken cancellationToken)
     {
         logger.LogInformation($"GetProductsByCategoryQueryHandler.Handle called with {query}");
 
         var products = await session.Query<Product>().Where(p => p.Category.Contains(query.Category)).ToListAsync();
 
         if (products is null)
-            throw new NotFoundException($"Product not found with Category : {query.Category}");
+            throw new NotFoundException($"Products not found with Category : {query.Category}");
 
         return new GetProductsByCategoryQueryResult(products);
     }
