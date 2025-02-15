d88587dd84c7bde080e561340f985103d8f88f96
sajjad0057 <sajjadhossain0057@gmail.com>
2025-02-15 16:04:40 +0600

    adding ShoppingCart and ShoppingCartItem models

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x=> x.Price * x.Quantity);
+
+    /// <summary>
+    /// Needing these empty constructor for mapping.
+    /// when we declar parameterized constructor then inactivated default constructor
+    /// </summary>
+    public ShoppingCart()
+    {
+    }
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+}
