6d212edf28ee7de9f0d02a50ac6c0425e2085774
sajjad0057 <sajjadhossain0057@gmail.com>
2025-02-16 21:44:38 +0600

    install Microsoft.Extensions.Caching.StackExchangeRedis package in Basket.API project and following decorator pattern creating CachedBasketRepository and in this using IDistributedCache interfaces

---
+++ src/Services/Basket/Basket.API/Repositories/CachedBasketRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Basket.API.Repositories;
+
+public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache)
+    : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasketAsync(string userName, CancellationToken cancellationToken = default)
+    {
+        var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
+
+        if(!string.IsNullOrEmpty(cachedBasket))
+            return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+
+        var basket = await repository.GetBasketAsync(userName, cancellationToken);
+
+        await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+
+        return basket;
+    }
+
+    public async Task<ShoppingCart> StoreBasketAsync(ShoppingCart cart, CancellationToken cancellationToken = default)
+    {
+        await repository.StoreBasketAsync(cart, cancellationToken);
+
+        await cache.SetStringAsync(cart.UserName, JsonSerializer.Serialize(cart), cancellationToken);
+
+        return cart;
+    }
+
+    public async Task<bool> DeleteBasketAsync(string userName, CancellationToken cancellationToken = default)
+    {
+        await repository.DeleteBasketAsync(userName, cancellationToken);
+
+        await cache.RemoveAsync(userName, cancellationToken);
+
+        return true;
+    }
+}
