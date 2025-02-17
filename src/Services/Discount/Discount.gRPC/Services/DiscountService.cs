697465b82dc86e475bd6187b05c3a04376a905fe
sajjad0057 <sajjadhossain0057@gmail.com>
2025-02-17 14:56:08 +0600

    creating DiscountService class initally

---
+++ src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
@@ -0,0 +1,35 @@
+﻿using Discount.gRPC.Protos;
+using Grpc.Core;
+
+namespace Discount.gRPC.Services;
+
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, 
+        ServerCallContext context)
+    {
+        //// TODO : GetDiscount From Database
+        return base.GetDiscount(request, context);
+    }
+
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, 
+        ServerCallContext context)
+    {
+        //// TODO : CreateDiscount To Database
+        return base.CreateDiscount(request, context);
+    }
+
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, 
+        ServerCallContext context)
+    {
+        //// TODO : UpdateDiscount To Database
+        return base.UpdateDiscount(request, context);
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, 
+        ServerCallContext context)
+    {
+        //// TODO : DeleteDiscount From Database
+        return base.DeleteDiscount(request, context);
+    }
+}
