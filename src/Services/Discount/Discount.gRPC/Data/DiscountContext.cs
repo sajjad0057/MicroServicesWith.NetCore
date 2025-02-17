9d2e10f874f486bf30493e613b6f59a1816d3128
sajjad0057 <sajjadhossain0057@gmail.com>
2025-02-17 15:14:39 +0600

    install Microsoft.EntityFrameworkCore.Sqlite package for using sqlite and create a dbcontext class for using EF core

---
+++ src/Services/Discount/Discount.gRPC/Data/DiscountContext.cs
@@ -0,0 +1,14 @@
+﻿using Discount.gRPC.Modules;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.gRPC.Data;
+
+public class DiscountContext : DbContext
+{
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+
+    public DiscountContext(DbContextOptions<DiscountContext> options) 
+        : base(options)
+    {          
+    }
+}
--- src/Services/Discount/Discount.gRPC/Program.cs
+++ src/Services/Discount/Discount.gRPC/Program.cs
@@ -1,3 +1,4 @@
+using Discount.gRPC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,12 @@ builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+#region Configure the HTTP request pipeline. 
+app.MapGrpcService<DiscountService>();
+
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client." +
+" To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+#endregion
+
 
 app.Run();
