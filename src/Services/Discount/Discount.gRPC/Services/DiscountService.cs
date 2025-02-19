78cdba348ea9a65163616f3fc8c6a7c3ca68eab0
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-02-19 16:14:21 +0600

    successfully done CRUD with gRPC

--- src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
+++ src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
@@ -42,17 +42,37 @@ public class DiscountService(DiscountContext dbContext, ILogger<DiscountService>
         return coupon.Adapt<CouponModel>();
     }
 
-    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, 
+    public async override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, 
         ServerCallContext context)
     {
         //// TODO : UpdateDiscount To Database
-        return base.UpdateDiscount(request, context);
+        var coupon = request.Coupon.Adapt<Coupon>();
+        if (coupon is null)
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request object."));
+
+        dbContext.Coupons.Update(coupon);
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation($"Discount is successfully updated. ProductName : {coupon.ProductName}");
+
+        return coupon.Adapt<CouponModel>();
     }
 
-    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, 
+    public async override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, 
         ServerCallContext context)
     {
         //// TODO : DeleteDiscount From Database
-        return base.DeleteDiscount(request, context);
+        var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+        if (coupon is null)
+            throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName} is not found."));
+
+        dbContext.Coupons.Remove(coupon);
+
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation($"Discount is successfully deleted. ProductName : { request.ProductName}");
+
+        return new DeleteDiscountResponse { Success = true };
     }
 }
