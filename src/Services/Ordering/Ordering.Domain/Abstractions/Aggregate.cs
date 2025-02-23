11951b3921106303b0cc0a5417a4326b0e86a66e
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-02-23 23:47:21 +0600

    02. Develop DDD Abstractions (Entity, Aggregate, DomainEvent) in Ordering.Domain project to follow CA

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Ordering.Domain.Abstractions;
+
+public abstract class Aggregate<T> : Entity<T>, IAggregate<T>
+{
+    public readonly List<IDomainEvent> _domainEvents = new();
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+        _domainEvents.Clear();
+        return dequeuedEvents;
+    }
+}
+
---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntiry.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Ordering.Domain.Abstractions;
+
+
+public interface IEntity<T> : IEntiry
+{
+    public T Id { get; set; }
+}
+
+
+public interface IEntiry
+{
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModifiedAt { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
+
+
+
