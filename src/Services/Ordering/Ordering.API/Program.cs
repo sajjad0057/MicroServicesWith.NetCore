bd87d0ea31370390dae566ab6a8242dd9ff95067
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-02-23 22:39:29 +0600

    Using extension methods adding dependency injections between all clean architecture layers in Program.cs file

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,7 +1,15 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-//// Add services to the container 
+//// Add services to the container using extension methods 
 
+builder.Services
+    .AddApplicationService()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiService();
 
 var app = builder.Build();
 
