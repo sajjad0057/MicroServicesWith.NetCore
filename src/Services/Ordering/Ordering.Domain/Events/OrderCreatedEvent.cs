6dc41358b1dbaa25ae0624c192e4a6f7c9278391
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-02-24 20:49:39 +0600

    Develop Order Created and Updated Domain Event in Ordering Microservices

---
+++ src/Services/Ordering/Ordering.Domain/Events/OrderCreatedEvent.cs
@@ -0,0 +1,8 @@
+﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Domain.Events;
+
+//// here IDomainEvent is an interface that inherite INotification interface also that's from MediatR library
+
+public record OrderCreatedEvent(Order Order) : IDomainEvent;
