e222df63afcfc73981ba567e6fc9b51fad37268e
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-03-03 03:09:22 +0600

    adding seed data

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -14,6 +14,11 @@ builder.Services
 var app = builder.Build();
 
 ////Configure the HTTP request pipeline
+app.UseApiServices();
 
+if (app.Environment.IsDevelopment())
+{
+    //await app.InitialiseDatabaseAsync();
+}
 
 app.Run();
