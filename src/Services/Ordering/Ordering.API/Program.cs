2f123f0fcbb733184a431fcb3454b6b7b360cd4d
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-03-03 03:09:38 +0600

    adding seed data

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +19,7 @@ app.UseApiServices();
 
 if (app.Environment.IsDevelopment())
 {
-    //await app.InitialiseDatabaseAsync();
+    await app.InitialiseDatabaseAsync();
 }
 
 app.Run();
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure.Data.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static async Task InitialiseDatabaseAsync(this WebApplication app)
+    { 
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+        await SeedDataAsync(context);
+    }
+
+    private static async Task SeedDataAsync(ApplicationDbContext context)
+    {
+        await SeedCustomersAsync(context);
+        await SeedProductsAsync(context);
+        await SeedOrderAndItemsAsync(context);
+    }
+
+    private static async Task SeedCustomersAsync(ApplicationDbContext context)
+    {
+        if (!await context.Customers.AnyAsync())
+        {
+            await context.Customers.AddRangeAsync(InitialData.Customers);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedProductsAsync(ApplicationDbContext context)
+    {
+        if (!await context.Products.AnyAsync())
+        {
+            await context.Products.AddRangeAsync(InitialData.Products);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedOrderAndItemsAsync(ApplicationDbContext context)
+    {
+        if (!await context.Orders.AnyAsync())
+        {
+            await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+            await context.SaveChangesAsync();
+        }
+    }
+}
