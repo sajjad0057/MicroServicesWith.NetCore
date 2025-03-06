2db00eb3c6c4c01bfa942ca2b5c9679c459ea775
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-03-07 02:46:04 +0600

    manage all endpoints for Ordering.API using carter

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,4 +1,9 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints;
 
 /*
 TODO : 
@@ -7,6 +12,27 @@ TODO :
 - Send the command using MediatR
 - Returns a success or not found response
 */
-public class DeleteOrder
+
+//public record DeleteOrderRequest(Guid Id);
+
+public record DeleteOrderResponse(bool IsSuccess);
+
+public class DeleteOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id}", async (Guid Id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteOrderCommand(Id));
+            var response =  result.Adapt<DeleteOrderResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteOrder")
+        .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete Order")
+        .WithDescription("Delete Order");
+    }
 }
--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -1,4 +1,10 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Carter;
+using Mapster;
+using MediatR;
+using Ordering.Application.Dtos;
+using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+namespace Ordering.API.Endpoints;
 
 
 /*
@@ -7,6 +13,27 @@ TODO :
 - Construct a GetOrdersByCustomerQuery with these parameters
 - Retrives and returns matching orders with peginated format
 */
-public class GetOrdersByCustomer
+
+//public record GetOrdersByCustomerRequest(Guid CustomerId);
+public record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+
+
+public class GetOrdersByCustomer : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByCustomerQuery(customerId));
+            var response = result.Adapt<GetOrdersByCustomerResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetOrdersByCustomer")
+        .Produces<GetOrdersByCustomerResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Orders By Customer")
+        .WithDescription("Get Orders By Customer");
+    }
 }
