85112892401a3822079c24a096fe51c77d0f2bb0
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-03-09 01:33:31 +0600

    Add MassTransit extension classes for manage configurations

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Configurations/MassTransit/Extensions.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Messaging.Configurations.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration)
+    {
+        //// TODO: put configuration with service container
+
+        #region Implement RabbitMQ MassTransit configuration
+
+        #endregion
+
+
+        return services;
+    }
+}
