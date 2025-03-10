1d90c95d7deaf16cbd6abd8b1214619b786c5a66
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-03-10 15:31:32 +0600

    updated port for YarpApiGateway project

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+//// Configure the HTTP request pipleline
 app.Run();
