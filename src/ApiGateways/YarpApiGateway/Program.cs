97eb41ac64cd639d1d15958a9cd2ffa2dd4836f7
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-03-10 16:12:10 +0600

    Develop Yarp Reverse Proxy in YarpApiGateway Microservices and test with basic configurations

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,8 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+#region Configuring Reverse proxy with YARP (Yet Another Reverse Proxy)
+//// Configuring YARP with DI container
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+#endregion
+
+
 //// Add services to the container
 var app = builder.Build();
 
 
 //// Configure the HTTP request pipleline
+#region Configuration Reverse proxy with http request pipeline
+app.MapReverseProxy();
+#endregion
+
 app.Run();
